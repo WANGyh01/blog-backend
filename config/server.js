280a038a489d507e7d00a747250b6c19a37b33c8
yuheng <yuheng@microdreams.com>
2021-08-15 16:57:05 +0800

    commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a7389b38d3a5b35cb16c1cd68508a377'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb389b0f-6f8f-4377-a945-f894bd870265'
+};
